b4368427aac04ae16951fbd8f081f2df7c512c7f
obokhove <o.bokhove@leeds.ac.uk>
2020-01-18 10:04:05 +0000

    Update readme.m

--- FiredrakeWaterWaves/CouplingBeachJune2018/readme.m
+++ FiredrakeWaterWaves/CouplingBeachJune2018/readme.m
@@ -1,3 +1,3 @@
 Coupled numerical wave tank; coupling potential-flow water-wave dynamics to shallow-water dynamics.
 
-The two data folders used are available upon request.
+The two data folders used are available upon request; one uploaded as zip, "experimental data" one is too big.
